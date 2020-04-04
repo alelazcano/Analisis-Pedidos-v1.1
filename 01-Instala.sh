d30467c3998752b00dd5215c4a8096d0c3399c9e
Victor <webmaster@regalonatural.com>
2020-04-04 21:29:30 +0200

    Ajustes instalacion

---
+++ 01-Instala.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+chmod +x /home/MTTO-TIENDA/Analisis-Pedidos/docker/montadocker.sh
+chmod +x /home/MTTO-TIENDA/Analisis-Pedidos/inicializa_sistema.sh
+
+bash /home/MTTO-TIENDA/Analisis-Pedidos/docker/montadocker.sh
+bash /home/MTTO-TIENDA/Analisis-Pedidos/inicializa_sistema.sh
--- docker/montadocker.sh
+++ docker/montadocker.sh
@@ -1,7 +1,5 @@
-#docker run -d -p 7070:80 -p 7071:3306  -v /home/UTILES/Analisis-Pedidos/www:/var/www/html -v /home/UTILES/Analisis-Pedidos/bbdd:/var/lib/mysql --name mysql-analisis-pedidos -h analisispedidos vicsoft01/ubuntu18.04-apache2.2-php7.3.11
-
 mkdir -p ../bbdd
-
+chmod 777 ../bbdd
 docker stop analisis-pedidos-phpMyAdmin
 docker rm  analisis-pedidos-phpMyAdmin
 docker stop analisis-pedidos-www
@@ -13,10 +11,8 @@ docker rm  analisis-pedidos-bbdd
 
 
 
-#docker run -d -p 7071:3306 -v /home/UTILES/Analisis-Pedidos/bbdd:/var/lib/mysql --name analisis-pedidos-bbdd -h db-analisispedidos mysql:5.7
-#docker run -d -p 7071:3306 -e --secure-file-priv="/var/lib/mysql" -v /home/UTILES/Analisis-Pedidos/bbdd:/var/lib/mysql --name analisis-pedidos-bbdd -h db-analisispedidos mysql:5.7
-#docker run -d -p 7071:3306 -v /home/UTILES/Analisis-Pedidos/bbdd/ficheros-sec-mysql:/var/lib/mysql-files/ -v /home/UTILES/Analisis-Pedidos/bbdd:/var/lib/mysql --name analisis-pedidos-bbdd -h db-analisispedidos mysql:5.7
-docker run -d -p 7071:3306 -v /home/MTTO-TIENDA/Analisis-Pedidos/bbdd:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=vmsn2004 --name analisis-pedidos-bbdd -h db-analisispedidos mysql:5.7
+docker run -d -p 7071:3306 -v /home/MTTO-TIENDA/Analisis-Pedidos/bbdd:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=vmsn2004 -e MYSQL_DATABASE=RN_AnalisisPedidos -e MYSQL_USER=victor -e MYSQL_PASSWORD=vmsn2004 --name analisis-pedidos-bbdd -h db-analisispedidos mysql:5.7 --default-authentication-plugin=mysql_native_password
 docker run -d -p 7070:80 -v /home/MTTO-TIENDA/Analisis-Pedidos/www:/var/www/html --name analisis-pedidos-www -h analisispedidos --link analisis-pedidos-bbdd:db vicsoft01/ubuntu18.04-apache2.2-php7.3.11
 docker run -d -p 7072:80 --name analisis-pedidos-phpMyAdmin -h admin-db-analisispedidos --link analisis-pedidos-bbdd:db phpmyadmin/phpmyadmin
+
 
