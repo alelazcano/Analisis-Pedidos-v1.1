e972c5b4a2416866a12d17e6dfd182e512dec577
Victor <webmaster@regalonatural.com>
2020-04-05 20:20:09 +0200

    Incluidas las notas de cliente y privadas en informe-

--- 01-Instala.sh
+++ 01-Instala.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+chmod +x /home/MTTO-TIENDA/Analisis-Pedidos/docker/montadocker.sh
 chmod +x /home/MTTO-TIENDA/Analisis-Pedidos/inicializa_sistema.sh
 chmod +x /home/MTTO-TIENDA/Analisis-Pedidos/recupera_pedidos.sh
 
